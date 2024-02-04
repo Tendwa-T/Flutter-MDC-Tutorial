6f81e7525e97a3c63df2184bfeacb2a6a143264c
Tendwa-T <tirus301@outlook.com>
2024-02-04 20:51:43 +0300

    Final Application

---
+++ lib/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/product.dart';
+import 'model/products_repository.dart';
+import 'supplemental/asymmetric_view.dart';
+
+class HomePage extends StatelessWidget {
+  final Category category;
+  const HomePage({Key? key, this.category = Category.all}) : super(key: key);
+
+  // TODO: Add a variable for Category (104)
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return AsymmetricView(products: ProductsRepository.loadProducts(category));
+  }
+}
